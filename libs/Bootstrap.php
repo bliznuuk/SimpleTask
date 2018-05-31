2af26fb7d96be351330d2650fc4411b6e0475ca4
vitaliy <bliznuuk@gmail.com>
2018-05-31 22:32:34 +0300

    Separate requestsCounter() function to the Statistics.php

--- libs/Bootstrap.php
+++ libs/Bootstrap.php
@@ -1,26 +1,16 @@
 <?php
+
 session_start();
-/**
- *
- */
+
 class Bootstrap
 {
 
   function __construct()
   {
     echo 'REQUEST_URI: ' . $_SERVER['REQUEST_URI'] . '<br>';
-    $this->requestsCounter();
-  }
 
-  private function requestsCounter()
-  {
-    if (!isset($_SESSION['requestsCounter']))
-    {
-      echo "session not set";
-      $_SESSION['requestsCounter'] = 0;
-    }
-
-    echo  '<br>Requests counter: ' . ++$_SESSION['requestsCounter'];
+    require_once 'Statistic.php';
+    Statistic::requestsCounter();
   }
 
 }
---
+++ libs/Statistic.php
@@ -0,0 +1,20 @@
+<?php
+
+class Statistic
+{
+
+  /**
+  * Return number of requests in current session
+  */
+  public static function requestsCounter(){
+    if (!isset($_SESSION['requestsCounter']))
+    {
+      echo "session not set";
+      $_SESSION['requestsCounter'] = 0;
+    }
+
+    echo  '<br>Requests counter: ' . ++$_SESSION['requestsCounter'];
+  }
+}
+
+ ?>
